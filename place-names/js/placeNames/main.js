42c41ce3792fa2240b46988b48a378f85c7b5147
Kiersten White <kierstenswhite@gmail.com>
2023-05-23 14:29:43 -0400

    Place Names C# to JavaScript

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -5,8 +5,17 @@ const names = [
     "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
 ];
 
-console.log("All Pace Names");
+console.log("All Place Names");
 
 for (const name of names) {
   console.log(name);
+}
+
+console.log("") //Line Break
+console.log("'The' Place Names");
+
+const theNames = names.filter(n => n.startsWith("The"))
+
+for (const theName of theNames) {
+console.log(theName)
 }
